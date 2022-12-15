7ee0fc92cb71fafe6500632f077a301e3b07a728
minister.ik <minister.ik@aptner.com>
2022-12-15 21:52:52 +0900

    initialize

---
+++ src/main/java/com/objects/objects/ObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.objects.objects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/objects/objects/ObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objects.objects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
