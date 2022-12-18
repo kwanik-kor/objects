6270dff8d35cd9208102120c49f40f06d79a8bf9
kwanik.kor <rhksdlr134@naver.com>
2022-12-18 17:35:50 +0900

    Chapter 1. TicketOffice 캡슐화하기(#1)

--- src/main/java/com/objects/objects/ticketSeller/TicketSeller.java
+++ src/main/java/com/objects/objects/ticketSeller/TicketSeller.java
@@ -11,7 +11,7 @@ public class TicketSeller {
     }
 
     public void sellTo(Audience audience) {
-        ticketOffice.plusAmount(audience.buy(ticketOffice.getTicket()));
+        ticketOffice.sellTicketTo(audience);
     }
 
 }
